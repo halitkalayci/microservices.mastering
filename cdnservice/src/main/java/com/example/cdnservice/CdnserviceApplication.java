f52380c86cb0f27db05fe2da9bf12cc256bf92fc
halitkalayci <halit.kalayci@ext.etiya.com>
2024-07-03 11:18:41 +0300

    add xmlhttprequest

---
+++ cdnservice/src/main/java/com/example/cdnservice/CdnserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cdnservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdnserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdnserviceApplication.class, args);
+	}
+
+}
